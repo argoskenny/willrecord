2aab742cf123a9df13b265331962cf308b1a1969
Kenny <argoskenny@gmail.com>
2015-04-30 16:54:49 +0800

    template change version 0.3 height adjust

--- assets/js/willrecord/public.js
+++ assets/js/willrecord/public.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
 	$('#footer_scroll_to_top').click(function() {
 		$("html,body").animate({scrollTop:0},400);
 	});
+	var main_container_height = $(window).height() - 210;
+	if ( $('.main .container').height() < main_container_height)
+	{
+		$('.main .container').css('height', main_container_height);
+	};
 });
